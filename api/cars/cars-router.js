058602059b2e25feb84df5c3470bd996e74f570b
MichaelMcFaddenjr <michaelpmcfaddenjr@gmail.com>
2021-06-08 21:50:28 -0600

    mvp

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,21 +1,40 @@
-const express = require('express')
-const Car = require('./cars-model')
+const express = require("express");
+const Car = require("./cars-model");
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+} = require("./cars-middleware");
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    const cars = await Car.getAll()
-    res.json(cars)
+    const cars = await Car.getAll();
+    res.json(cars);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
-router.get('/', async (req, res, next) => {
-  res.json('getting car with id')
-})
-router.post('/', async (req, res, next) => {
-  res.json('posting new car')
-})
+});
 
-module.exports = router
+router.get("/:id", checkCarId, async (req, res, next) => {
+  res.json(req.car);
+});
+
+router.post(
+  "/",
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+  async (req, res, next) => {
+    try {
+      const car = await Car.create(req.body);
+      res.json(car);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,22 +1,22 @@
-const express = require("express")
-const carsRouter = require('./cars/cars-router')
 
-const server = express()
+const express = require("express");
+const carsRouter = require("./cars/cars-router");
 
-server.use(express.json())
+const server = express();
 
-server.use('/api/cars', carsRouter)
+server.use(express.json());
 
-server.use('*', (req, res, next) => {
-  next({ status:404, message:'not found '})
-})
+server.use("/api/cars", carsRouter);
 
-server.use((err, req, res, next) => { //eslint-disable-line
+server.use("*", (req, res, next) => {
+  next({ status: 404, message: "not found" });
+});
+
+server.use((err, req, res, next) => {
+  //eslint-disable-line
   res.status(err.status || 500).json({
-    message: err.message
-  })
-})
+    message: err.message,
+  });
+});
 
-// DO YOUR MAGIC
-
-module.exports = server
+module.exports = server;
